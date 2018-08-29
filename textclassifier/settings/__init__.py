f65dec3433bbb0d88552b2535b753a95b16d3e8c
Aditya Kaushik <adityakaushik471@gmail.com>
2018-08-30 02:03:44 +0530

    production test-1

--- textclassifier/settings/__init__.py
+++ textclassifier/settings/__init__.py
@@ -1,7 +1,7 @@
-from .base import  *
+# from .base import  *
 from .production import *
 
-try:
-    from .local import *
-except:
-    pass
+# try:
+#     from .local import *
+# except:
+#     pass

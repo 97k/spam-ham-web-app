d31a4dac097759331a034b12a5c4aaf996f11e11
Aditya Kaushik <adityakaushik471@gmail.com>
2018-08-30 13:29:09 +0530

    fix text_process on user input

--- spamham/views.py
+++ spamham/views.py
@@ -16,6 +16,7 @@ def home(request):
         message = request.POST.get('message')
         message = [message]
         message = text_process(message)
+        message = [' '.join(message)]
         result, accuracy = predict(message)
         return render(request, 'home.html', {'result': result, 'message': message[0], 'accuracy': accuracy})
 

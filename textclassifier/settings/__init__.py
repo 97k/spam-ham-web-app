465a7c4c29627f26eccf0e2d31545ded6a7e98e0
Aditya Kaushik <adityakaushik471@gmail.com>
2018-08-30 10:33:25 +0530

    fixed allowed host on testing production-4

--- textclassifier/settings/__init__.py
+++ textclassifier/settings/__init__.py
@@ -2,7 +2,7 @@ from .base import  *
 from .production import *
 from utils import  text_process
 
-try:
-    from .local2 import *
-except:
-    pass
+# try:
+#     from .local2 import *
+# except:
+#     pass
